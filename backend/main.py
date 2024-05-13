749fb8875b8c3a2a53f71fc4927f340f17ded1d4
Matija Bažec <mb5525@student.uni-lj.si>
2024-05-13 20:13:17 +0200

    backend lots o stuff

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Depends, Form, WebSocket
 from pydantic import BaseModel
-import uuid
+import os
+from mlflow import MlflowClient
+from dotenv import load_dotenv
+from utils.execute_ssh import execute_ssh_command
+from utils.s3_upload_file import s3_upload_file
+import boto3
+from botocore.exceptions import ClientError
+from botocore.client import Config
+from fastapi.responses import HTMLResponse
+from utils.rabbitmq import RabbitMQ
+
+
 
 
 # request body for model
@@ -8,14 +19,28 @@ class ModelInput(BaseModel):
     model_name: str
     input_string: str  | None = None
 
+
+# method to make enviroment variables avalable
+load_dotenv()
+
+# startup the FastAPI app
 app = FastAPI()
 
 
-# get all the models 
+# get all models
 @app.get("/api/v1/models")
 async def get_all_models():
-    #TODO Get all the models and return 
-    return {"message": "TODO get all models"}
+
+    try:
+        client = MlflowClient()
+        list_of_models = [dict(rm) for rm in client.search_model_versions()]
+
+        return {"models": list_of_models}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to connect to MLflow server")
+
+
 
 
 #post a phrase and train the model
@@ -26,41 +51,61 @@ async def train_model(input_data: ModelInput):
     model_name = input_data.model_name
     input_string = input_data.input_string
 
+    # execute command via custom method and return to api the anwser from the ssh (or error)
+    output = execute_ssh_command(os.environ["COMMAND_TRAIN"])
 
-    #TODO train the model wiht the fraze, retrun confirmation when done
 
-    #return status message
-    return {"message": "TODO train model"}
+    
 
+    if output is not None:
+        print("Command output:")
+        print(output)
+        return {"message": output}
+    else:
+        return {"message": "An error has occured"}
 
 
 
 #post an image and have the model classiy it
 @app.post("/api/v1/model/classify")
-async def classify_image(model_input: ModelInput, file: UploadFile = File(...)):
+async def classify_image(model_input: ModelInput = Depends(), file: UploadFile = File(...)):
+
+    # upload image
+    filename = s3_upload_file(file)
 
 
-    file.filename = f"{uuid.uuid4()}.jpg"
+    # execute command via custom method and return to api the anwser from the ssh (or error)
+    output = execute_ssh_command('bash hackathon/hpc/bin/mobilenetv3.sh')
 
-    contents = await file.read()
 
-    #TODO
-
-    #return name of image
-    return {"filename": file.filename}
+    if output is not None:
+        print("Command output:")
+        print(output)
+        return {"message": output}
+    else:
+        return {"message": "An error has occured"}
 
 
 
 #generate llm answers
 @app.post("/api/v1/llm/generate")
-async def llm_generate(input_data: ModelInput):
-    # Extract the model name and input string from the request body
-    model_name = input_data.model_name
-    input_string = input_data.input_string
-    #TODO train the model wiht the fraze, retrun confirmation when done
+async def llm_generate(prompt = str):
+    print(f'bash hackathon/hpc/bin/alpaca_llm.sh {prompt}')
+    # execute command via custom method and return to api the anwser from the ssh (or error)
+    output = execute_ssh_command(f'bash hackathon/hpc/bin/alpaca_llm.sh {prompt}')
 
-    #return status message
-    return {"message": "TODO train model"}
+    rabbit = RabbitMQ(routing_key="#")
+    rabbit.receive()
+
+    
+
+    if output is not None:
+        print("Command output:")
+        print(output)
+        return {"message": output}
+    else:
+        return {"message": "An error has occured"}
+
 
 
 
@@ -71,5 +116,16 @@ async def llm_generate(input_data: ModelInput):
 
 #generate llm embeding of image
 @app.post("/api/v1/llm/embedding")
-async def llm_embeding(input_data: ModelInput):
-    return {"message": "TODO train model"}
+async def llm_embeding(file: UploadFile = File(...)):
+
+    # upload image
+    filename = s3_upload_file(file)
+
+    # execute command via custom method and return to api the anwser from the ssh (or error)
+    output = execute_ssh_command('bash hackathon/hpc/bin/mobilenetv3.sh')
+    if output is not None:
+        print("Command output:")
+        print(output)
+        return {"message": output}
+    else:
+        return {"message": "An error has occured"}
